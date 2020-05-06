80d33e46fb87992c24f1b8af0b75f49bc9d1de42
Lucas <xz351@hotmail.fr>
2020-05-06 17:24:35 +0200

    Added style

--- js/components/todoElement.js
+++ js/components/todoElement.js
@@ -1,9 +1,14 @@
 export default function TodoElement(outlet, data) {
   outlet.innerHTML = '';
   const constructor = document.createElement('li');
+  constructor.classList = "max-w-sm w-full lg:max-w-full lg:flex rounded bg-white mb-4 hover:shadow transition-all duration-200 ease-out"
   constructor.innerHTML = `
-    <span>${data.content}</span>
-    <input type="checkbox" value="${data.done}">
+    <div class="p-4 border-r border-solid border-gray-300">
+      <input type="checkbox" value="${data.done}">
+    </div>
+    <div class="p-4 flex flex-col justify-center">
+      <span>${data.content}</span>
+    </div>
   `
   outlet.appendChild(constructor);
   return constructor;
--- js/views/Todolist.js
+++ js/views/Todolist.js
@@ -4,9 +4,13 @@ export default function Todolist(outlet, data) {
   outlet.innerHTML = '';
   const constructor = document.createElement('div');
   constructor.innerHTML = `
-    <section class="h-full" name="todolist">
-      <h1>Todolist: ${data.todolist.name}</h1>
+    <section class="h-full">
+      <h1 class="text-2xl font-semibold m-4 text-center">${data.todolist.name}</h1>
       <ul name="todolistElements"></ul>
+      <form name="addTodo" class="w-full flex hover:shadow transition-all duration-200 ease-out">
+        <input type="text" name="todoContent" class="w-full rounded outline-none p-2 rounded-br-none rounded-tr-none">
+        <button class="bg-blue-300 outline-none rounded px-3 text-xl text-white rounded-tl-none rounded-bl-none">+</button>
+      </form>
     </section>
   `;
   for(const element of data.todolist.elements) {
